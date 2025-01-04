d13c4be718fdcc423b7398c746ce9efcf04d3523
ZoltanKertesz <kertesz.zoltan.9718@gmail.com>
2025-01-04 20:53:10 +0100

    login api endpoint is working

--- src/controllers/authentication.ts
+++ src/controllers/authentication.ts
@@ -4,9 +4,53 @@ import { getUserByEmail , createUser} from '../models/User';
 import {authentication, random} from '../helpers/index';
 import { Connect,CloseConnect} from "../configs/databaseConnect";
 
+export const login = async (req: Request, res: Response)=>{
+    await Connect();
+    try {
+        const {email, password} = req.body;
+        if (!email || !password) {
+            CloseConnect();
+            res.status(400).send("email and password are required");
+        }
+
+        const user = await getUserByEmail(email).select('+authentication.password +authentication.salt');
+
+        if(!user){
+            CloseConnect();
+            res.status(400).send("Invalid e-mail address");
+            return;
+        }
+
+        if (!user?.authentication?.salt) {
+            CloseConnect();
+            res.status(403).send("Invalid e-mail address");
+            return;
+        }
+        const exceptedHash = authentication( password,user.authentication.salt);
+
+        if (user.authentication.password !== exceptedHash) {
+            CloseConnect();
+            res.status(403).send("Invalid password");
+        }
+
+        const salt = random();
+        user.authentication.sessionToken = authentication(user._id.toString(),salt);
+        await user.save();
+
+        CloseConnect();
+        res.cookie('SESSION-TOKEN', user.authentication.sessionToken, {domain: 'localhost', path: '/'}).status(200).json(user);
+        
+    } catch (error) {
+        CloseConnect();
+        res.status(400).send("Login not successfull");
+        console.log(error);
+    }
+
+}
+
 
 export const register = async (req: Request, res: Response) => {  
-    Connect();
+    await Connect();
     try {
         const { username, email, password } = req.body;
         if (!username || !email || !password) {
